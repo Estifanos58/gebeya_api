0d9ddf7d4110edfc6e04d06f5df4e6969bb00982
Estifanos58 <estifkebe08@gmail.com>
2025-08-14 15:28:44 +0300

    test(auth): login test added

--- src/auth/handlers/get-user.handler.ts
+++ src/auth/handlers/get-user.handler.ts
@@ -1,19 +1,14 @@
 import { IQueryHandler, QueryHandler } from "@nestjs/cqrs";
 import { GetUserQuery } from "../queries/get-user-query";
-import { InjectRepository } from "@nestjs/typeorm";
-import { User } from "@/entities";
-import { Repository } from "typeorm";
 import { HttpException, HttpStatus } from "@nestjs/common";
 
 @QueryHandler(GetUserQuery)
 export class GetUserHandler implements IQueryHandler<GetUserQuery>{
-    constructor(
-        @InjectRepository(User) private readonly userRepo: Repository<User>
-    ){}
+    constructor(){}
     async execute(query: GetUserQuery): Promise<any> {
         const {user} = query; 
 
-        if(!user){
+        if(!user || Object.keys(user).length === 0){
             throw new HttpException({ message: "User Not Found"}, HttpStatus.NOT_FOUND)
         }
         return {
